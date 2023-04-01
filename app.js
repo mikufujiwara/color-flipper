fceadf344acadaff1e05994064df061576ba8342
mikufujiwara <llt9.oct22@gmail.com>
2023-03-31 23:52:16 -0700

    hex added

--- app.js
+++ app.js
@@ -33,8 +33,13 @@ const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 const reset1 = document.getElementById("reset1");
 const reset2 = document.getElementById("reset2");
+const hex1 = document.getElementById("hex1");
+const hex2 = document.getElementById("hex2");
+const delay = 500;
+let timeoutID;
 
 const vColorHandler = () => {
+  clearTimeout(timeoutID);
   const randomV = Math.floor(Math.random() * vividColors.length);
   const pickVcolor = vividColors[randomV];
   left.style.background = pickVcolor;
@@ -42,6 +47,8 @@ const vColorHandler = () => {
 };
 
 const pColorHandler = () => {
+
+  clearTimeout(timeoutID);
   const randomP = Math.floor(Math.random() * pastelColors.length);
   const pickPcolor = pastelColors[randomP];
   right.style.background = pickPcolor;
@@ -49,13 +56,35 @@ const pColorHandler = () => {
 };
 
 const resetHandler1 = () => {
-    left.style.background = "white";
-}
+
+  clearTimeout(timeoutID);
+  left.style.background = "white";
+  colorCode1.innerHTML = "#ffffff";
+};
 const resetHandler2 = () => {
-    right.style.background = "white";
-}
+
+  clearTimeout(timeoutID);
+  right.style.background = "white";
+  colorCode2.innerHTML = "#ffffff";
+};
+
+const hexhandler1 = () => {
+  const randomV = Math.floor(Math.random() * vividColors.length);
+  const pickVcolor = vividColors[randomV];
+  left.style.background = pickVcolor;
+  timeoutID = setTimeout(hexhandler1, delay);
+};
+
+const hexhandler2 = () => {
+  const randomP = Math.floor(Math.random() * pastelColors.length);
+  const pickPcolor = pastelColors[randomP];
+  right.style.background = pickPcolor;
+  timeoutID = setTimeout(hexhandler2, delay);
+};
 
 btn1.addEventListener("click", vColorHandler);
 btn2.addEventListener("click", pColorHandler);
 reset1.addEventListener("click", resetHandler1);
 reset2.addEventListener("click", resetHandler2);
+hex1.addEventListener("click", hexhandler1);
+hex2.addEventListener("click", hexhandler2);
