4a1560d2263c62a7dcddd709792246c537ce91f6
mikufujiwara <llt9.oct22@gmail.com>
2023-03-31 22:54:58 -0700

    updated

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
-const colors = [
-  "#6495ed",
-  "#4682b4",
-  "#f4a460",
-  "#cd5c5c",
-  "#6a5acd",
-  "#3cb371",
-  "#2e8b57",
-  "#d2b48c",
-  "#deb887",
-  "#add8e6",
-  "#ffa07a",
+const vividColors = [
+  "#F90D1B",
+  "#FE6006",
+  "#FDE005",
+  "#EC00FC",
+  "#9D00FE",
+  "#00CF35",
+  "#D91D81",
+  "#D91D81",
+  "#D91D81",
+  "#FF0000",
+  "#00FFFF",
 ];
-let main = document.querySelector("main");
-let colorCode = document.querySelector("span");
-const btn = document.querySelector("h2");
+const pastelColors = [
+  "#ca9bf7",
+  "#ff9899",
+  "#ffb7ce",
+  "#fdfd96",
+  "#f4bfff",
+  "#c8ffb0",
+  "#d6fffe",
+  "#cef0cc",
+  "#aa9499",
+  "#bdb0d0",
+  "#deece1",
+];
 
-const bgColorChanger = () => {
-    const randomNum = Math.floor(Math.random() * colors.length);
-    const pickColor = colors[randomNum];
-    main.style.background = pickColor;
-    colorCode.innerHTML = pickColor
+let left = document.getElementById("left");
+let right = document.getElementById("right");
+let colorCode1 = document.getElementById("colorcode1");
+let colorCode2 = document.getElementById("colorcode2");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const reset1 = document.getElementById("reset1");
+const reset2 = document.getElementById("reset2");
+
+const vColorHandler = () => {
+  const randomV = Math.floor(Math.random() * vividColors.length);
+  const pickVcolor = vividColors[randomV];
+  left.style.background = pickVcolor;
+  colorCode1.innerHTML = pickVcolor;
 };
 
-btn.addEventListener("click", bgColorChanger);
+const pColorHandler = () => {
+  const randomP = Math.floor(Math.random() * pastelColors.length);
+  const pickPcolor = pastelColors[randomP];
+  right.style.background = pickPcolor;
+  colorCode2.innerHTML = pickPcolor;
+};
+
+const resetHandler1 = () => {
+    left.style.background = "white";
+}
+const resetHandler2 = () => {
+    right.style.background = "white";
+}
+
+btn1.addEventListener("click", vColorHandler);
+btn2.addEventListener("click", pColorHandler);
+reset1.addEventListener("click", resetHandler1);
+reset2.addEventListener("click", resetHandler2);
